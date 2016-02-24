1e1aa85ae3f477e40861832c555d3dfaf766721d
Pantalleresco <a100694@M016-13.mcast.org.mt>
2016-02-24 09:46:59 +0100

    Game+Exe File

---
+++ Assets/scripts/buttonManager.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class buttonManager : MonoBehaviour {
+
+
+
+	public void next(){
+		StartCoroutine (DoFadeNext());
+	}
+
+
+	IEnumerator DoFadeNext (){
+		
+		CanvasGroup canvasGroup = GetComponent<CanvasGroup>();
+
+			while (canvasGroup.alpha>0){
+				canvasGroup.alpha -= Time.deltaTime * 2;
+
+				yield return null;
+			}
+
+			canvasGroup.interactable = false; // makes invisble buttons unclickable
+
+			yield return new WaitForSeconds (0.5f);
+			Application.LoadLevel(Application.loadedLevel + 1);
+
+			yield return null;
+		}
+
+	public void previous(){		
+		StartCoroutine (DoFadePrevious());
+	}
+
+	IEnumerator DoFadePrevious (){
+		
+		CanvasGroup canvasGroup = GetComponent<CanvasGroup>();
+		
+		while (canvasGroup.alpha>0){
+			canvasGroup.alpha -= Time.deltaTime * 2;
+			
+			yield return null;
+		}
+		
+		canvasGroup.interactable = false; // makes invisble buttons unclickable
+		
+		yield return new WaitForSeconds (0.5f);
+		Application.LoadLevel(Application.loadedLevel - 1);
+		
+		yield return null;
+	}
+
+	}
+
+	
